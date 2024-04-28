504bf80f5bdc49b27df6cca19eaaf34d44baec7b
banchiec <icaro_19@hotmail.com>
2024-04-28 23:25:50 +0200

    feature/footer-desktop

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,26 @@
 import { FooterType } from "./types"
+import instagram from "../../assets/images/instagram.png"
+import pinterest from "../../assets/images/pinterest.png"
+import linkedIn from "../../assets/images/linkedin.png"
+import "./footer.scss"
 
 const Footer = ( props: FooterType) => {
 	return(
 		<div className="klz-footer">
 			<div className="klz-footer-container">
 				<div className="klz-footer__social-media">
-					footer
+					<div className="klz-footer__social-media__icon">
+						<img src={instagram} alt="Instagram"/>
+					</div>
+					<div className="klz-footer__social-media__icon">
+						<img src={pinterest} alt="Pinterest"/>
+					</div>
+					<div className="klz-footer__social-media__icon">
+						<img src={linkedIn} alt="LinkedIn"/>
+					</div>
+				</div>
+				<div className="klz-footer__contactme">
+					<h4>klausjohannes@art.com</h4>
 				</div>
 			</div>
 		</div>
