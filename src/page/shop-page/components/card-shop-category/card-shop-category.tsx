7e0f344076511a4828b3713d7f3f1e6e6d7cc766
banchiec <icaro_19@hotmail.com>
2024-05-05 14:22:16 +0200

    feature/shop-page

--- src/page/shop-page/components/card-shop-category/card-shop-category.tsx
+++ src/page/shop-page/components/card-shop-category/card-shop-category.tsx
@@ -1,14 +1,23 @@
-import klzAboutMe from "../../../../assets/images/about-me-klauz.jpeg"
+import { CardShopCategoryType } from "./types"
 import "./card-shop-category.scss"
-export const CardShopCategory = () => {
+import { Link } from "react-router-dom"
+export const CardShopCategory = (props: CardShopCategoryType) => {
+	const { title, description, path, image } = props
 	return(
 		<div className="klz-card-shop-category">
+			<Link to={path!}>
 			<div className="klz-card-shop-category-image">
-				<img src={klzAboutMe} alt="image_category"/>
+				<img src={image} alt="image_category"/>
 			</div>
-			<div>
-				<p></p>
+			<div className="klz-card-shop-category-data">
+				<div className="klz-card-shop-category__title">
+					<h4>{title.toUpperCase()}</h4>
+				</div>
+				<div className="klz-card-shop-category__description">
+					<p>{description}</p>
+				</div>
 			</div>
+			</Link>
 		</div>
 	)
 }
--- src/page/shop-page/shop-page.tsx
+++ src/page/shop-page/shop-page.tsx
@@ -1,12 +1,36 @@
 import CardShopCategory from "./components/card-shop-category"
 import "./shop-page.scss"
+import StuffCantBuy from "../../assets/images/stuff-you-cant-buy.png"
+import  Commisions from "../../assets/images/commissions.png"
+
+const categoryItems = [
+	{
+		title: "Stuff you can buy",
+		description: "I'm selling illustrations, stickers, and shirts here – everything that I love, and hopefully, you will too!",
+		path: "/",
+		image: StuffCantBuy
+	},
+	{
+		title: "Commisions",
+		description: "I'm selling illustrations, stickers, and shirts here – everything that I love, and hopefully, you will too!",
+		path: "/",
+		image: Commisions 
+	}
+] 
 
 export const ShopPage = () => {
 	return(
 		<div className="klz-shop-page">
 			<div className="klz-shop-page-container">
-				<CardShopCategory/>
-				<CardShopCategory/>
+				{categoryItems.map((category, index) =>(
+					<CardShopCategory  
+						key={index}
+						title={category.title} 
+						path={category.path} 
+						description={category.description}
+						image={category.image}
+					/>
+				))}
 			</div>
 		</div>
 	)
