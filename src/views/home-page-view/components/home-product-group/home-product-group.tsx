1c21bac29423ff6c9452b7c49b749455bde4e35f
banchiec <icaro_19@hotmail.com>
2024-11-27 20:27:54 +0100

    feature/home-page

--- src/views/home-page-view/components/home-product-group/home-product-group.tsx
+++ src/views/home-page-view/components/home-product-group/home-product-group.tsx
@@ -8,55 +8,64 @@ const {products} = props;
 const firstThree = products.products.slice(0, 3);
 const nextThree = products.products.slice(3, 6);
 const lastProducts = products.products.slice(0, 2);
-
     return (
-		<div className="klz-portfolio-group-card-home">
-		<div className="klz-portfolio-group-card-home__container1"> 
-			{ firstThree.map((product: {_id: string, image: string}, indice: number) =>{
-				return (
-					<div key={product._id} className="klz-portfolio-group-card-product-card">
-						<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
-							<ProductCard key={indice} product={product} />
-						</Link>
-					</div>
-				)
-			})}
+		<div className="klz-portfolio-product-home-group">
+			<div className="klz-portfolio-product-home-group__container"> 
+				{products?.products.map((product: any, indice: number) =>{
+					return (
+						<div key={product._id} className="klz-portfolio-product-home-group__container-product">
+							<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
+								<ProductCard key={indice} product={product} />
+							</Link>
+						</div>)
+				})}
+			 {/* 	{ firstThree.map((product: {_id: string, image: string}, indice: number) =>{
+					return (
+						<div key={product._id} className="klz-portfolio-group-card-product-card">
+							<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
+								<ProductCard key={indice} product={product} />
+							</Link>
+						</div>
+					)
+				})}
+			*/}
+			</div>
+			{/**
+			 * 
+			<div className="klz-portfolio-group-card-home__container2"> 
+				{ nextThree.map((product: {_id: string, image: string}, indice: number) =>{
+					return (
+						<div key={product._id} className="klz-portfolio-group-card-product-card">
+							<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
+								<ProductCard key={indice} product={product} />
+							</Link>
+						</div>
+					)
+				})}
+			</div>
+			<div className="klz-portfolio-group-card-home__container3"> 
+				{ nextThree.map((product: {_id: string, image: string}, indice: number) =>{
+					return (
+						<div key={product._id} className="klz-portfolio-group-card-product-card">
+							<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
+								<ProductCard key={indice} product={product} />
+							</Link>
+						</div>
+					)
+				})}
+			</div>
+			<div className="klz-portfolio-group-card-home__container4"> 
+				{ lastProducts.map((product: {_id: string, image: string}, indice: number) =>{
+					return (
+						<div key={product._id} className="klz-portfolio-group-card-product-card">
+							<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
+								<ProductCard key={indice} product={product} />
+							</Link>
+						</div>
+					)
+				})}
+			</div>
+			 */}
 		</div>
-		<div className="klz-portfolio-group-card-home__container2"> 
-			{ nextThree.map((product: {_id: string, image: string}, indice: number) =>{
-				return (
-					<div key={product._id} className="klz-portfolio-group-card-product-card">
-						<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
-							<ProductCard key={indice} product={product} />
-						</Link>
-					</div>
-				)
-			})}
-		</div>
-
-		<div className="klz-portfolio-group-card-home__container3"> 
-			{ nextThree.map((product: {_id: string, image: string}, indice: number) =>{
-				return (
-					<div key={product._id} className="klz-portfolio-group-card-product-card">
-						<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
-							<ProductCard key={indice} product={product} />
-						</Link>
-					</div>
-				)
-			})}
-		</div>
-
-		<div className="klz-portfolio-group-card-home__container4"> 
-			{ lastProducts.map((product: {_id: string, image: string}, indice: number) =>{
-				return (
-					<div key={product._id} className="klz-portfolio-group-card-product-card">
-						<Link to={`/shop/stuff-you-can-buy/${product._id}`}>
-							<ProductCard key={indice} product={product} />
-						</Link>
-					</div>
-				)
-			})}
-		</div>
-	</div>
-    )
+ 	)
 }
