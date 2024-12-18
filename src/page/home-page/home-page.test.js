d226cd38cbf7532e27a8ad5da305453f88277597
Lexadiaz120 <daura38@gmail.com>
2024-12-18 23:43:51 +0100

    add first tests

--- src/page/home-page/home-page.test.js
+++ src/page/home-page/home-page.test.js
@@ -2,15 +2,26 @@ import "@testing-library/jest-dom/extend-expect";
 import { render } from "@testing-library/react";
 import { HomePage } from "./home-page";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+
+jest.mock("./home-page");
 
 test("renders content", () => {
   const queryClient = new QueryClient();
 
-  const component = render(
+  render(
     <QueryClientProvider client={queryClient}>
       <HomePage />
     </QueryClientProvider>
   );
+});
 
-  console.log(component);
+describe("renders content", () => {
+  test("check the home page route", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <HomePage />
+      </MemoryRouter>
+    );
+  });
 });
